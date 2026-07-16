3884224bb336c54c5993c7a4540bb2da61bc58d5
Atonu <susnigdho1234@gmail.com>
2026-07-16 20:58:58 +0600

    Refactor code structure for improved readability and maintainability

--- Home/models.py
+++ Home/models.py
@@ -6,6 +6,8 @@ class AudioMediations(models.Model):
     duration = models.IntegerField()
     audio_file = models.FileField(upload_to='mediation_audios/')
     source = models.CharField(max_length=255, blank=True, null=True)
+    language = models.CharField(max_length=20)
+    
     
     def __str__(self) :
         return self.title
