4541879f0c606b370fe7c252e6f1d6a93d756329
Atonu <susnigdho1234@gmail.com>
2026-07-20 13:07:32 +0600

    Implement profile update functionality with additional fields and redirect after saving

--- User_Auth/views.py
+++ User_Auth/views.py
@@ -1,9 +1,27 @@
-from django.shortcuts import render
-
-# Create your views here.
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import *
+
 
 # Create your views here.
 def profile(request):
-    return render(request, "profile.html")
+    profile = request.user.profile
+    languages = Language.objects.all()
+    if request.method == 'POST':
+        profile.full_name = request.POST.get('full_name')
+        profile.email = request.POST.get('email')
+        profile.gender = request.POST.get('gender')
+        profile.nationality = request.POST.get('nationality')
+        profile.birthday = request.POST.get('birthday')
+        profile.language = request.POST.get('language')
+        
+        profile.save()
+        return redirect('profile')
+        
+    data = {
+        'profile' : profile,
+        'languages' : languages,
+        
+        
+    }
+    
+    return render(request, "profile.html", data)
