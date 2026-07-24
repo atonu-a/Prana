ebe9e3766ab946be978a5eb57e20e4dedd56b710
Atonu <susnigdho1234@gmail.com>
2026-07-24 22:15:27 +0600

    Add profile button for authenticated users and implement password toggle functionality

--- User_Auth/urls.py
+++ User_Auth/urls.py
@@ -2,7 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('profile/',views.profile, name="profile"),
+
     path('login-page/',views.login_view, name="login_view"),
+    path('profile/',views.profile, name="profile"),
     
 ]
--- static/js/app.js
+++ static/js/app.js
@@ -13,6 +13,37 @@ mobileMenuBtn.addEventListener("click", () => {
   }
 });
 
+// Password eye button
+function togglePassword() {
+  const passwordInput = document.getElementById("password");
+  const toggleIcon = document.getElementById("toggleIcon");
+
+  if (passwordInput.type === "password") {
+    passwordInput.type = "text";
+    toggleIcon.classList.remove("fa-eye");
+    toggleIcon.classList.add("fa-eye-slash");
+  } else {
+    passwordInput.type = "password";
+    toggleIcon.classList.remove("fa-eye-slash");
+    toggleIcon.classList.add("fa-eye");
+  }
+}
+
+//Some button clicks
+
+// sign in (header) button
+const signinBtn = document.getElementById("signin-btn");
+signinBtn?.addEventListener("click", (e)=> {
+  const url = signinBtn.getAttribute('data-link');
+  window.location.href = url;
+})
+// profile (header) button
+const profileBtn = document.getElementById("profile-btn");
+profileBtn?.addEventListener("click", (e)=> {
+  const url = profileBtn.getAttribute('data-link');
+  window.location.href = url;
+})
+
 // like Dislike button
 const likes = document.querySelectorAll(".like");
 likes.forEach((like) => {
@@ -28,25 +59,10 @@ likes.forEach((like) => {
         heart.classList.add("fa-regular");
       }
 
-      setTimeout(()=>{
-        heart.classList.remove("fa-beat")
-      },800)
+      setTimeout(() => {
+        heart.classList.remove("fa-beat");
+      }, 800);
     }
   });
 });
 
-
- function togglePassword() {
-            const passwordInput = document.getElementById('password');
-            const toggleIcon = document.getElementById('toggleIcon');
-
-            if (passwordInput.type === 'password') {
-                passwordInput.type = 'text';
-                toggleIcon.classList.remove('fa-eye');
-                toggleIcon.classList.add('fa-eye-slash');
-            } else {
-                passwordInput.type = 'password';
-                toggleIcon.classList.remove('fa-eye-slash');
-                toggleIcon.classList.add('fa-eye');
-            }
-        }
